3e2e06ca3d3b0b934e6c68c4ca5947118cd2b06f
DoThiKhanh <khanhdo22091999@gmail.com>
2020-10-11 09:07:54 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
-import { NgModule } from '@angular/core';
+import { DetailComponent } from './product/detail/detail.component';
+import { MainComponent } from './main/main.component';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { combineAll } from 'rxjs-compat/operator/combineAll';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { LoginComponent } from './customer/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then((m) => m.ProductModule),
+  },
+
+  {
+    path: 'customer',
+    loadChildren: () => import('./customer/customer.module').then((m) => m.CustomerModule),
+  },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+
+   {
+     path: '',
+     component: MainComponent,
+   },
+
+   {
+     path: 'home',
+     component: MainComponent,
+   },
+
+
+   {
+     path: 'detail',
+     component: DetailComponent,
+   },
+   {
+     path: 'login',
+     component: LoginComponent,
+   },
+   {
+    path: '**',
+    component: NotFoundComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { FooterComponent } from './shared/footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainComponent } from './main/main.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CartComponent } from './customer/cart/cart.component';
+import { CheckoutComponent } from './customer/checkout/checkout.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
