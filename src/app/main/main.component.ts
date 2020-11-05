04bd1a8235d3bb6250234e3e6f71a4cdc4b34bd6
DoThiKhanh <khanhdo22091999@gmail.com>
2020-11-05 19:50:43 +0700

    Update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -20,7 +20,7 @@ export class MainComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.list_item=[];
     this.index=1;
-    this.size=4;
+    this.size=8;
     let elem = document.getElementsByClassName("script");
     for(var i = elem.length -1; 0 <= i; i--) {
       elem[i].remove();
